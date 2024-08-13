d6d056addfabc833eaaa7b2bab4575327824654c
Lorenzopedro.freitas <98405763+lorenzopedro@users.noreply.github.com>
2024-08-13 23:29:46 +0000

    Pending changes exported from your codespace

--- aula01/Program.cs
+++ aula01/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Data;
 
 public class Program{
     public static void main(string[] args){
         //Cadastrar os dados de Pessoa
         Console.WriteLine("Cadastro de pessoa!");
         Console.Write("Nome: ");
-        string nomePessoa = Console.ReadLine();
+        string? nomePessoa = Console.ReadLine();
 
         Console.WriteLine("Idade: ");
         int idadePessoa = int.Parse(Console.ReadLine());
 
+        Pessoa pessoa1 = new Pessoa(nomePessoa, idadePessoa);
+
+        Console.WriteLine(pessoa1.ToString());
     }
 }
