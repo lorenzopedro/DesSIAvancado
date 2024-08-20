dc299f23e1353f600d054a33161e044d4eaa73a1
Lorenzopedro.freitas <98405763+lorenzopedro@users.noreply.github.com>
2024-08-20 23:30:56 +0000

    Código da aula 20-08-2024

---
+++ aula04/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program{
+        public static void Main(string[] args){
+            Pessoa objPessoa = new Pessoa ("Lorenzo");
+            Console.WriteLine("Antes do método (Classe): " + objPessoa.ToString());
+            AlteraNomeClasse(objPessoa);    
+            Console.WriteLine("Depois do método (Classe): " + objPessoa.ToString());
+
+            PessoaStruct pessoaStruct = new PessoaStruct("Kauan");
+            Console.WriteLine("Antes do método (Struct): " + pessoaStruct.ToString());
+            AlteraNomeStruct(pessoaStruct);
+            Console.WriteLine("Depois do método (Struct): " + pessoaStruct.ToString());
+        }
+
+        public static void AlteraNomeClasse(Pessoa objAux){
+            objAux.Nome = "Amanda";
+        }
+
+        public static void AlteraNomeStruct(PessoaStruct objAux){
+            objAux.NomeStruct = "Leonin";
+        }
+}
