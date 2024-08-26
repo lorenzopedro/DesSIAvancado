b94b709e5fb70457af71a816ef776dc27824e02c
Lorenzopedro.freitas <98405763+lorenzopedro@users.noreply.github.com>
2024-08-26 23:51:49 +0000

    Commitando coisa atoa

--- aula01/obj/Debug/net8.0/aula01.AssemblyInfo.cs
+++ aula01/obj/Debug/net8.0/aula01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8380db5c5bfd8ac862aefa7c050744f95d1abf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc299f23e1353f600d054a33161e044d4eaa73a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
