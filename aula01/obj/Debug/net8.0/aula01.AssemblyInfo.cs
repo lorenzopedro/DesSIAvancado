bf7e69043d1a00dfdceca960a5742fc791668b4d
Lorenzopedro.freitas <98405763+lorenzopedro@users.noreply.github.com>
2024-08-27 00:33:18 +0000

    26-08-2024

--- aula01/obj/Debug/net8.0/aula01.AssemblyInfo.cs
+++ aula01/obj/Debug/net8.0/aula01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc299f23e1353f600d054a33161e044d4eaa73a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b94b709e5fb70457af71a816ef776dc27824e02c")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
