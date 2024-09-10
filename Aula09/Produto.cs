95eb4272cab7c3ba0390f30b6a34d648e51bdaaa
Lorenzopedro.freitas <98405763+lorenzopedro@users.noreply.github.com>
2024-09-10 23:29:14 +0000

    Aula 10/09/2024

---
+++ Aula09/Produto.cs
@@ -0,0 +1,30 @@
+public class Produto{
+    private int id;
+    private string nome = "";
+    private decimal preco;
+
+    public int Id{
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string Nome{
+        get { return nome; }
+        set { nome = value; }
+    }
+
+    public decimal Preco{
+        get { return preco; }
+        set { preco = value; }
+    }
+
+    public Produto(int id, string nome, decimal preco){
+        Id = id;
+        Nome = nome;
+        Preco = preco;
+    }
+
+    public override string ToString(){
+        return $"Id: {Id}, Nome: {Nome}, Preço: {Preco}";
+    }
+}
---
+++ Aula09/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program{
+    public static void Main(){
+        //Criando clientes
+        Cliente cliente1 = new Cliente(1, "João");
+        Cliente cliente2 = new Cliente(2, "Maria");
+
+        //Criando os produtos
+        Produto produto1 = new Produto(1, "Notebook", 3500.00M);
+        Produto produto2 = new Produto(2, "Smartphone", 1800.00M);
+        Produto produto3 = new Produto(3, "Tablet", 1200.00M);
+        Produto produto4 = new Produto(4, "Fone de Ouvido", 150.00M);
+
+        //Criando compra para cliente 1
+        Compra compra1 = new Compra(cliente1);
+        compra1.AdicionaProduto(produto1);
+        compra1.AdicionaProduto(produto2);
+        compra1.AdicionaProduto(produto4);
+
+        //Criando compra para cliente 2
+        Compra compra2 = new Compra(cliente2);
+        compra2.AdicionaProduto(produto3);
+        compra2.AdicionaProduto(produto1);
+        compra2.AdicionaProduto(produto2);
+
+        //Exibir as compras
+        Console.WriteLine(compra1);
+        Console.WriteLine(compra2);
+    }
+}
